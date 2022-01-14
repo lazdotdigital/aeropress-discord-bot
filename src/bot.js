4f8f57aa6af110807adc57c6ebac75c13c976d2c
Laz H <lazdotdigital@gmail.com>
2022-01-14 08:43:40 -0800

    delete role messages

--- src/bot.js
+++ src/bot.js
@@ -5,13 +5,16 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', async message => {
   if (message.author.bot) return;
 
   if (message.content.startsWith('addrole')) {
     const roleName = message.content.split(' ')[1];
     const role = getRole(roleName, message.guild.roles.cache);
-    if (role) message.member.roles.add(role);
+    if (role) {
+      await message.member.roles.add(role);
+      await message.delete();
+    }
   }
 });
 
